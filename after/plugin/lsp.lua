7747c756a6db4970492968ae2ef52a0a72a31e57
Julian Vesper <julian.vesper@ruhr-uni-bochum.de>
2023-02-28 03:51:28 +0000

    Replace tabs with four spaces in files in after directory

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -3,26 +3,26 @@ local cmp = require('cmp')
 
 lsp.preset('recommended')
 lsp.ensure_installed({
-	'rust_analyzer',
+    'rust_analyzer',
 })
 
 lsp.set_preferences({
-	sign_icons = {
-		error = 'E',
-		warn  = 'W',
-		hint  = 'H',
-		info  = 'I',
-	}
+    sign_icons = {
+        error = 'E',
+        warn  = 'W',
+        hint  = 'H',
+        info  = 'I',
+    }
 })
 
 local cmp_select = { behavior = cmp.SelectBehavior.Select }
 local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-k>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-j>'] = cmp.mapping.select_next_item(cmp_select),
 })
 
 lsp.setup_nvim_cmp({
-	mapping = cmp_mappings,
+    mapping = cmp_mappings,
 })
 
 -- TODO: Insert ThePrimeagen remaps here

f1253d5d51fd5f5689dabcb37ad39a4253a49aa0
Julian Vesper <julian.vesper@ruhr-uni-bochum.de>
2023-03-01 16:26:51 +0000

    Add working harpoon keymaps

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,7 +6,7 @@ vim.keymap.set('n', '<leader>a', mark.add_file, { desc = "Mark file" })
 vim.keymap.set('n', '<C-e>', ui.toggle_quick_menu, { desc = "Open harpoon quick menu" })
 
 -- Shortcuts for first four files
-vim.keymap.set('n', '<C-h>', function() ui.nav_file(1) end, { desc = "Open harpoon file at slot 1" })
-vim.keymap.set('n', '<C-j>', function() ui.nav_file(2) end, { desc = "Open harpoon file at slot 2" })
-vim.keymap.set('n', '<C-n>', function() ui.nav_file(3) end, { desc = "Open harpoon file at slot 3" })
-vim.keymap.set('n', '<C-m>', function() ui.nav_file(4) end, { desc = "Open harpoon file at slot 4" })
+vim.keymap.set('n', '<leader>h', function() ui.nav_file(1) end, { desc = "Open harpoon file at slot 1" })
+vim.keymap.set('n', '<leader>j', function() ui.nav_file(2) end, { desc = "Open harpoon file at slot 2" })
+vim.keymap.set('n', '<leader>k', function() ui.nav_file(3) end, { desc = "Open harpoon file at slot 3" })
+vim.keymap.set('n', '<leader>l', function() ui.nav_file(4) end, { desc = "Open harpoon file at slot 4" })

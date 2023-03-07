bde0cfc0135fe4d63f25cc87396e5ea15d5bc6e5
Julian V <jvesper95@gmail.com>
2023-03-07 03:02:03 +0100

    Add LSP keymap descriptions

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,7 +7,7 @@ lsp.ensure_installed({
     'rust_analyzer',
 })
 
--- Fix Undefined global 'vim'
+-- Fix undefined global 'vim' error
 lsp.configure('lua-language-server', {
     settings = {
         Lua = {
@@ -39,19 +39,19 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-    local opts = {buffer = bufnr, remap = false}
+    local opts = { buffer = bufnr, remap = false}
 
-    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, opts)
-    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set('n', '<leader>vf', function() vim.lsp.buf.format() end, opts)
-    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set('n', 'gd', function() vim.lsp.buf.definition() end, { buffer = bufnr, remap = false, desc = "Jump to definition" })
+    vim.keymap.set('n', 'K', function() vim.lsp.buf.hover() end, { buffer = bufnr, remap = false, desc = "Display hover info" })
+    vim.keymap.set('n', '<leader>vws', function() vim.lsp.buf.workspace_symbol() end, { buffer = bufnr, remap = false, desc = "Rename buffer" })
+    vim.keymap.set('n', '<leader>vd', function() vim.diagnostic.open_float() end, { buffer = bufnr, remap = false, desc = "Open diagnostic float" })
+    vim.keymap.set('n', '[d', function() vim.diagnostic.goto_next() end, { buffer = bufnr, remap = false, desc = "Jump to next code diagnostic" })
+    vim.keymap.set('n', ']d', function() vim.diagnostic.goto_prev() end, { buffer = bufnr, remap = false, desc = "Jump to previous code diagnostic" })
+    vim.keymap.set('n', '<leader>vca', function() vim.lsp.buf.code_action() end, { buffer = bufnr, remap = false, desc = "Show code actions" })
+    vim.keymap.set('n', '<leader>vrr', function() vim.lsp.buf.references() end, { buffer = bufnr, remap = false, desc = "Show references" })
+    vim.keymap.set('n', '<leader>vrn', function() vim.lsp.buf.rename() end, { buffer = bufnr, remap = false, desc = "Rename buffer" })
+    vim.keymap.set('n', '<leader>vf', function() vim.lsp.buf.format() end, { buffer = bufnr, remap = false, desc = "Format file" })
+    vim.keymap.set('i', '<C-h>', function() vim.lsp.buf.signature_help() end, { buffer = bufnr, remap = false, desc = "Show signature help" })
 end)
 
 lsp.setup()
